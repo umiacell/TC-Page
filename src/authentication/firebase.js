9ea3762bf75381edc1a7bc78496af77cacddfca5
NoeCanoNunez <noecano2013@gmail.com>
2023-07-10 12:37:37 -0500

    Change authentic

--- src/authentication/firebase.js
+++ src/authentication/firebase.js
@@ -5,11 +5,13 @@
     
 
     const firebaseConfig = {
-      apiKey: "AIzaSyAhzWqOjH7oZHPWBt48lsW-xXjOq81EBLQ",
-      authDomain: "projectauthtenloclaro.firebaseapp.com",
-      projectId: "projectauthtenloclaro",
-      storageBucket: "projectauthtenloclaro.appspot.com",
-      messagingSenderId: "307505070994",
+      apiKey: "AIzaSyDS7IHXKdKhDQW-4H_ndq2xaA37OhIK6lo",
+      authDomain: "tc-page-umia.firebaseapp.com",
+      projectId: "tc-page-umia",
+      storageBucket: "tc-page-umia.appspot.com",
+      messagingSenderId: "477755579311",
+      appId: "1:477755579311:web:db798633ba8149b947ed5a",
+      measurementId: "G-W0HW8YEHFD",
     };
 
     // Initialize Firebase
